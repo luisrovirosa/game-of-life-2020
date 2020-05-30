75d6fb3ae067ed817e5b072cd782e293787ca0c9
Luis Rovirosa Mairlot <luis@codium.team>
2020-05-30 21:08:41 +0200

    all logic from WorldBuilder moved into CellsBuilder

--- src/CellsBuilder.php
+++ src/CellsBuilder.php
@@ -8,8 +8,13 @@ class CellsBuilder
 {
     private $cells;
 
-    public function __construct(array $stringCells)
+    public function __construct(array $cells = null)
     {
+        $stringCells = $cells ?? [
+                ['.', '.', '.'],
+                ['.', '.', '.'],
+                ['.', '.', '.'],
+            ];
         $this->cells = array_map(fn(array $row): array => array_map(fn(string $cell): Cell => $cell === '*' ? Cell::alive() : Cell::dead(), $row), $stringCells);
     }
 
@@ -19,5 +24,26 @@ class CellsBuilder
     public function build(): array
     {
         return $this->cells;
+    }
+
+    public function aliveAt(int $row, int $col): self
+    {
+        $this->setCell($row, $col, Cell::alive());
+
+        return $this;
+    }
+
+    public function setCell(int $row, int $col, Cell $cell): self
+    {
+        $this->cells[$row][$col] = $cell;
+
+        return $this;
+    }
+
+    public function withAliveCells(array $neighbors): self
+    {
+        array_map(fn(array $neighbor) => $this->aliveAt($neighbor[0], $neighbor[1]), $neighbors);
+
+        return $this;
     }
 }
--- src/WorldBuilder.php
+++ src/WorldBuilder.php
@@ -6,42 +6,36 @@ namespace Katas;
 
 class WorldBuilder
 {
-    /** @var Cell[][] */
-    private array $cells;
+    private CellsBuilder $cellsBuilder;
 
     public function __construct(array $cells = null)
     {
-        $stringCells = $cells ?? [
-                ['.', '.', '.'],
-                ['.', '.', '.'],
-                ['.', '.', '.'],
-            ];
-        $this->cells = (new CellsBuilder($stringCells))->build();
+        $this->cellsBuilder = new CellsBuilder($cells);
     }
 
     public function aliveAt(int $row, int $col): self
     {
-        $this->setCell($row, $col, Cell::alive());
+        $this->cellsBuilder->aliveAt($row, $col);
 
         return $this;
     }
 
     public function setCell(int $row, int $col, Cell $cell): self
     {
-        $this->cells[$row][$col] = $cell;
+        $this->cellsBuilder->setCell($row, $col, $cell);
+
+        return $this;
+    }
+
+    public function withAliveCells(array $neighbors): self
+    {
+        $this->cellsBuilder->withAliveCells($neighbors);
 
         return $this;
     }
 
     public function build(): World
     {
-        return new World($this->cells);
-    }
-
-    public function withAliveCells(array $neighbors): self
-    {
-        array_map(fn(array $neighbor) => $this->aliveAt($neighbor[0], $neighbor[1]), $neighbors);
-
-        return $this;
+        return new World($this->cellsBuilder->build());
     }
 }
