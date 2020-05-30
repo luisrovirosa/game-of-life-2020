7846bfc81dee7f5b36eb702d82a708a03f563905
Luis Rovirosa Mairlot <luis@codium.team>
2020-05-30 21:18:15 +0200

    NeighborFinderTest added second test at position 1,1

--- tests/NeighborFinderTest.php
+++ tests/NeighborFinderTest.php
@@ -13,21 +13,26 @@ class NeighborFinderTest extends TestCase
     /**
      * @test
      * @dataProvider scenarios
+     * @param int $row
+     * @param int $col
+     * @param array $aliveNeighbors
+     * @param $expectedNeighbors
      */
-    public function find_the_number_of_alive_neighbors(int $row, int $col, array $aliveNeighbors): void
+    public function find_the_number_of_alive_neighbors(int $row, int $col, array $aliveNeighbors, $expectedNeighbors): void
     {
         $cells = (new CellsBuilder())->withAliveCells($aliveNeighbors)->build();
         $finder = new NeighborFinder($cells);
 
         $numberOfAliveNeighbors = $finder->numberOfAliveNeighbors($row, $col);
 
-        $this->assertEquals(0, $numberOfAliveNeighbors);
+        $this->assertEquals($expectedNeighbors, $numberOfAliveNeighbors);
     }
 
     public function scenarios(): array
     {
         return [
-            'at position 1,1 without neighbors' => [1, 1, []],
+            'at position 1,1 without neighbors' => [1, 1, [], 0],
+            'at position 1,1 with one neighbor' => [1, 1, [[0, 0]], 1],
         ];
     }
 }
