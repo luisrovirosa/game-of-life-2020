e5b4221600dce4ba77d8e534e63297e0c6df25c7
Luis Rovirosa Mairlot <luis@codium.team>
2020-05-30 21:41:43 +0200

    NeighborFinder extract method

--- src/NeighborFinder.php
+++ src/NeighborFinder.php
@@ -22,6 +22,15 @@ class NeighborFinder
 
     private function areNeighbors(int $row, int $col, int $rowNeighbor, int $colNeighbor): bool
     {
-        return !($rowNeighbor === $row && $colNeighbor === $col);
+        if ($this->isThemselves($rowNeighbor, $row, $colNeighbor, $col)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function isThemselves(int $rowNeighbor, int $row, int $colNeighbor, int $col): bool
+    {
+        return ($rowNeighbor === $row && $colNeighbor === $col);
     }
 }
